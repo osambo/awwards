1017c4726e900da8f382ea56b753b456e60194e3
kodhanjo <felix.osambo@student.moringaschool.com>
2021-09-16 13:49:16 +0300

    imported registration form

--- user/views.py
+++ user/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 def register(request):
