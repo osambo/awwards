bab8555ec824a505141e96f29395086a80272ae7
kodhanjo <felix.osambo@student.moringaschool.com>
2021-09-23 09:42:04 +0300

    installed pil

--- awwards/settings.py
+++ awwards/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+# STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, "static"),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
